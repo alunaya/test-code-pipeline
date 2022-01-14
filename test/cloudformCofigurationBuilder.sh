c83d1811f43446c4a3c5d27d45f58815518a9075
Tuanta52 <>
2022-01-14 16:43:23 +0700

    test

--- test/cloudformCofigurationBuilder.sh
+++ test/cloudformCofigurationBuilder.sh
@@ -1,6 +1,5 @@
 JSONSTRING=$( jq -n \
-    --arg BuildCommit "" \
-    '{Parameters: {BuildCommit: $BuildCommit}}'
+    '{Parameters: {BuildCommit: $CODEBUILD_RESOLVED_SOURCE_VERSION}}'
 )
 
 echo $JSONSTRING > ./CloudformConfiguration.json
