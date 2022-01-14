e25a7ea312e943b792896e103e9e6d36b7f7055d
Tuanta52 <>
2022-01-14 14:42:58 +0700

    sfasfd

--- test/lambda1/index.js
+++ test/lambda1/index.js
@@ -1,1 +1,5 @@
-console.log("lambda1");
+exports.handler =  async function(event, context) {
+    console.log("lambda1")
+    console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+    return context.logStreamName
+}
--- test/lambda2/index.js
+++ test/lambda2/index.js
@@ -1,1 +1,5 @@
-console.log("lambda2")
+exports.handler =  async function(event, context) {
+    console.log("lambda2")
+    console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+    return context.logStreamName
+}
