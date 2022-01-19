ef48924a480392e80f749fed744cb062a1e2223a
vagrant <vagrant@ubuntu2004.localdomain>
2022-01-19 02:34:07 +0000

    test

--- generate-script.sh
+++ generate-script.sh
@@ -2,4 +2,4 @@ TargetJsonString=$(jq --arg buildid "${CODEBUILD_RESOLVED_SOURCE_VERSION}" \
  '.Parameters.BuildIdParameter |= $buildid' ./test/cloudform-conf.json)
 TargetJsonString=$(jq --arg outputbucket "${OutputBucket}" \
  '.Parameters.OutputBucketParameter |= $outputbucket' <<< $TargetJsonString)
-echo $TargetJsonString | tee ./${service}/${CloudFormConfigurationFileName} > /dev/null
+echo $TargetJsonString | tee ./${service}/${CloudFormConfigurationFileName}
