1fb46f28ff4791041e3d2698c9b3ba756e9e6c5c
alunaya <shadowwoft@gmail.com>
2022-01-19 16:42:49 +0700

    test

--- generate-script.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-TargetJsonString=$(jq --arg buildid "${CODEBUILD_RESOLVED_SOURCE_VERSION}" \
- '.Parameters.BuildIdParameter |= $buildid' ./${service}/${CloudFormConfigurationFileName})
-TargetJsonString=$(echo $TargetJsonString | jq --arg outputbucket "${OutputBucket}" \
- '.Parameters.OutputBucketParameter |= $outputbucket')
-echo $TargetJsonString | tee ./${service}/${CloudFormConfigurationFileName} > /dev/null
-# echo '{"Parameters": {"BuildId":"'${CODEBUILD_RESOLVED_SOURCE_VERSION}'","OutputBucket":"'${OutputBucket}'"}}' | tee ./${service}/${CloudFormConfigurationFileName}.json
--- test/lambda1/index.js
+++ test/lambda1/index.js
@@ -1,4 +1,7 @@
+let helloWorld = require("hello-world-npm")
+
 exports.handler =  async function(event, context) {
+    console.log(helloWorld());
     console.log("lambda1")
     console.log("EVENT: \n" + JSON.stringify(event, null, 2) + " lambda1")
     return context.logStreamName
